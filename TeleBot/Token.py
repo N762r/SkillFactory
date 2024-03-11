595bef2660a1455ab9385a8595a6936741406a4a
N762r <sobolev.qz@yandex.ru>
2024-03-11 11:19:43 +0300

    Update Token.py

--- TeleBot/Token.py
+++ TeleBot/Token.py
@@ -1,12 +1,9 @@
-# токен telegram-бота
 TOKEN = "6996569466:AAHmM3hD_P156WCCEsJb0EunJKz-b9CzESU"
 
-# токен для API
 header = {
   "apikey": "5sSZyJZaBVdVCfjVC14SIHf677a8z45e"
 }
 
-# словарь доступных пользователю валют
 keys = {"рубль": "RUB",
         "евро": "EUR",
         "доллар": "USD",
