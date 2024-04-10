b450ded2f6bd80908376d22313253337784d495c
N762r <sobolev.qz@yandex.ru>
2024-04-11 02:50:47 +0300

    УРА

--- AsyncTeleBot/recipes_handler.py
+++ AsyncTeleBot/recipes_handler.py
@@ -11,32 +11,33 @@ from aiogram.utils.formatting import (
 )
 from aiogram.fsm.context import FSMContext
 from aiogram import Router, types
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 router = Router()
 
 @router.message(Command('categorysearchrandom'))
 async def cmd_search(message: Message, command: CommandObject, state: FSMContext):
-    if command.args is None:
-        await message.answer(
-            "Ошибка: не переданы аргументы"
-        )
+    if not command.args:
+        await message.reply("Пожалуйста, укажите число рецептов после команды.")
         return
 
-    await state.set_data({'num_recipes': int(command.args[0])})
+    try:
+        num_recipes = int(command.args[0])
+    except ValueError:
+        await message.reply("Неверный формат числа рецептов.")
+        return
+
+    await state.update_data(num_recipes=num_recipes)
 
     async with aiohttp.ClientSession() as session:
         async with session.get('http://www.themealdb.com/api/json/v1/1/list.php?c=list') as resp:
             categories_data = await resp.json()
-            categories = [cat['strCategory'] for cat in categories_data['meals']]
-    
-    kb_ct = [
-        []
-    ]
-    for category in categories:
-        kb_ct.append(types.KeyboardButton(text=f'{category}'))
-    keyboard = types.ReplyKeyboardMarkup(
-        keyboard=kb_ct,
-        resize_keyboard=True,
-    )
 
-    await message.reply("Выберите категорию рецепта:", reply_markup=keyboard)
+    keyboard_builder = ReplyKeyboardBuilder()
+    for category in categories_data['meals']:
+        category_name = category['strCategory']
+        keyboard_builder.add(types.KeyboardButton(text = category_name))
+    keyboard_builder.adjust(4)
+
+    await message.answer("Выберите категорию:", reply_markup=keyboard_builder.as_markup(resise_keyboard=True))
+    #await RecipeSearchStates.CategorySelection.set()
