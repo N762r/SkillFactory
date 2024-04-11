a4fe2739752f06864f88e3bd84a4029283946e08
N762r <sobolev.qz@yandex.ru>
2024-04-11 04:48:39 +0300

    Почти готово

--- AsyncTeleBot/recipes_handler.py
+++ AsyncTeleBot/recipes_handler.py
@@ -1,19 +1,23 @@
 import aiohttp
 import json
-
-from datetime import datetime
-
-from aiogram.filters import Command, CommandObject
+import random
+from random import choices
+from aiogram.filters import Command, CommandObject, StateFilter
+from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import Message
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
-from aiogram.utils.formatting import (
-   Bold, as_list, as_marked_section
-)
+from googletrans import Translator
 from aiogram.fsm.context import FSMContext
 from aiogram import Router, types
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 router = Router()
+translator = Translator()
+
+class RecipeSearchStates(StatesGroup):
+    category_selection = State()
+    getting_recipes = State()
+
 
 @router.message(Command('categorysearchrandom'))
 async def cmd_search(message: Message, command: CommandObject, state: FSMContext):
@@ -40,4 +44,40 @@ async def cmd_search(message: Message, command: CommandObject, state: FSMContext
     keyboard_builder.adjust(4)
 
     await message.answer("Выберите категорию:", reply_markup=keyboard_builder.as_markup(resise_keyboard=True))
-    #await RecipeSearchStates.CategorySelection.set()
+
+    await state.set_state(RecipeSearchStates.category_selection.state)
+
+
+@router.message(RecipeSearchStates.category_selection)
+async def handle_category_choice(message: Message, state: FSMContext):
+    chosen_category = message.text
+
+    url = f'https://www.themealdb.com/api/json/v1/1/filter.php?c={chosen_category}'
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status != 200:
+                await message.reply("Произошла ошибка при получении данных.")
+                return
+
+            data = await response.json()
+
+            if 'meals' not in data:
+                await message.reply("По выбранной категории нет рецептов.")
+                return
+
+            recipes = []
+            for meal in data['meals']:
+                rec = meal['strMeal']
+                r = translator.translate(f'{rec}', dest='ru')
+                recipes.append(r.text)
+
+            data = await state.get_data()
+            num_recipes = data.get("num_recipes")
+
+            random_recipes = random.choices(recipes, k=num_recipes)
+            random_recipes = ', '.join(random_recipes)
+            kb = [[types.KeyboardButton(text="Покажи рецепты"),],]
+            keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True,)
+
+            await message.answer(f'Как Вам такие варианты: {random_recipes}', reply_markup=keyboard)
--- AsyncTeleBot/token_data.py
+++ AsyncTeleBot/token_data.py
@@ -1,14 +1,1 @@
 TOKEN = '7100898536:AAHSIACamXvQq0pW7QVHOvDR5GqS8MGxCLk'
-# import asyncio
-# import aiohttp
-# import json
-
-# async def curr_request():
-#     async with aiohttp.ClientSession() as session:
-#         async with session.get('http://www.themealdb.com/api/json/v1/1/list.php?c=list') as resp:
-#             categories_data = await resp.json()
-#             categories = [cat['strCategory'] for cat in categories_data['meals']]
-#             for category in categories:
-#                 print(category, type(category))
-
-# asyncio.run(curr_request())
